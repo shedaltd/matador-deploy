7394ed2de3e7be79d5d96e8272477da411f28ac7
Timon Sotiropoulos <timon@seedtech.io>
2016-05-19 11:52:05 +1000

    Added a few more fixes to the help arguments

--- app/src/arguments.py
+++ app/src/arguments.py
@@ -25,24 +25,27 @@ def noSystemArgsExist(arguments):
     return len(arguments) == 1
 
 def printHelpDocumentationThenExit():
-    print "Ganado-Deploy"
-    print "Rancher Deployment Assitant Script\n"
+    print "Ganado-Deploy - Rancher Deployment Assitant Script\n"
+    print "Author: SEED Digital\n"
     print "This script will assist you in updating your rancher environment."
     print "All the following command line arguments are required to update rancher\n"
+    print "Version: v%s" % __version__
+    call(["./rancher/rancher-compose", "-v"])
+    print ""
     print "Usage: [-flags] --url <http://example.com> --key <key> --secret <secret_key> --env [dev|staging|prod]"
-    print "    --url \t url to connect to your rancher server"
-    print "    --key \t username of api key to connect to rancher host"
-    print "    --secret \t password of api key to connect to rancher host"
-    print "    --env \t environment you wish to update"
+    print "  --url \t url to connect to your rancher server"
+    print "  --key \t username of api key to connect to rancher host"
+    print "  --secret \t password of api key to connect to rancher host"
+    print "  --env \t environment you wish to update"
     print ""
-    print "    Flags [-hfvd]"
-    print "    -h  \t Show the help documentation -- (will stop application from running unless force mode is also present)"
-    print "    -f  \t Force Mode: force the application to run and supress all warnings"
-    print "    -v  \t Verbose Mode: print additional messages are processes run"
-    print "    -d  \t Development Mode: will bypass command line arguments and set default values for Rancher configuration"
+    print "  Flags [-hfvd]"
+    print "  -h  \t Show the help documentation -- (will stop application from running unless force mode is also present)"
+    print "  -f  \t Force Mode: force the application to run and supress all warnings"
+    print "  -v  \t Verbose Mode: print additional messages are processes run"
+    print "  -d  \t Development Mode: will bypass command line arguments and set default values for Rancher configuration"
     print ""
-    print "    Version Information"
-    print "    --version  \t Print out the current version of ganado-deploy and the local installed version of rancher-compose"
+    print "  Version Information"
+    print "  --version  \t Print out the current version of ganado-deploy and the local installed version of rancher-compose"
     sys.exit(0)
 
 def printVersionInformationThenExit():
